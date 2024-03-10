e8999053b27ce6737635710ae6b2de83967b7f47
Teddy Morin <morin.td@gmail.com>
2024-03-10 20:51:17 +0100

    feat: 🎸 complete play use case

--- src/tic-tac-toe/use-cases/play.use-case.ts
+++ src/tic-tac-toe/use-cases/play.use-case.ts
@@ -1,5 +1,9 @@
 import { Executable } from "../../common/use-case";
+import { Board } from "../entities/board.entity";
+import { CellAlreadyTakenException } from "../exceptions/cell-already-taken.exception";
+import { StepDoesNotExistException } from "../exceptions/step-does-not-exist.exception";
 import { GameDomainModel } from "../game.domain-model";
+import { IBoardRepository } from "../ports/board-repository.port";
 
 export type Input = {
   square: number;
@@ -14,5 +18,53 @@ export type Output = {
 };
 
 export class Play implements Executable<Input, Output> {
-  async execute(input: Input) {}
+  constructor(private boardRepository: IBoardRepository) {}
+
+  async execute(input: Input) {
+    const { square, step } = input;
+    const history = await this.boardRepository.findHistory();
+    const currentBoard = history[step];
+
+    if (!currentBoard) {
+      throw new StepDoesNotExistException();
+    }
+
+    const xIsNext = currentBoard.xIsNext();
+    const winner = currentBoard.calculateWinner();
+
+    if (winner) {
+      return {
+        winner,
+        history: history.map((board) => board.squares),
+        xIsNext,
+        step,
+      };
+    }
+
+    if (currentBoard.squares[square]) {
+      throw new CellAlreadyTakenException();
+    }
+
+    const board = new Board({
+      squares: currentBoard.squares.slice() as GameDomainModel.Squares,
+    });
+
+    if (xIsNext) {
+      board.squares[square] = GameDomainModel.Square.X;
+    } else {
+      board.squares[square] = GameDomainModel.Square.O;
+    }
+
+    this.boardRepository.setHistory([...history.slice(0, step + 1), board]);
+
+    return {
+      history: [
+        ...history.slice(0, step + 1).map((board) => board.squares),
+        board.squares,
+      ],
+      winner: board.calculateWinner(),
+      xIsNext: board.xIsNext(),
+      step: step + 1,
+    };
+  }
 }
